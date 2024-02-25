3220fde7ca554f95956da46f026f8d0386815c8a
selam-ben-samet <basabdussamed@gmail.com>
2024-02-25 14:31:32 +0300

    aiming with mouse added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,27 +6,46 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Rigidbody _rb;
-    [SerializeField] private float _speed=5;
+    [SerializeField] private float _speed = 5;
     [SerializeField] private float _turnSpeed = 360;
     private Vector3 _input;
-    void Update(){
+
+    void Update()
+    {
         GatherInput();
-        Look();
+        LookAtMouse();
     }
-    void FixedUpdate(){
+
+    void FixedUpdate()
+    {
         Move();
     }
-    void GatherInput(){
-    _input = new Vector3(Input.GetAxisRaw("Horizontal"),0,Input.GetAxisRaw("Vertical"));
+
+    void GatherInput()
+    {
+        _input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
     }
-    void Look(){
-        if(_input != Vector3.zero){
-        var relative = (transform.position + _input.ToIso()) - transform.position;
-        var rotation = Quaternion.LookRotation(relative,Vector3.up);
-        transform.rotation = Quaternion.RotateTowards(transform.rotation,rotation, _turnSpeed * Time.deltaTime);
-        }      
+
+    void LookAtMouse()
+    {
+        // Raycast from the camera to the mouse position
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            // Calculate the relative direction from the player position to the mouse position
+            Vector3 relative = hit.point - transform.position;
+            relative.y = 0; // Set the y-component to zero to keep the rotation on the same plane
+
+            // Rotate the player towards the relative direction
+            Quaternion rotation = Quaternion.LookRotation(relative, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, _turnSpeed * Time.deltaTime);
+        }
     }
-    void Move(){
-        _rb.MovePosition(transform.position+(transform.forward*_input.magnitude)*_speed*Time.deltaTime);
-    }
-}   
+
+    void Move()
+{
+    Vector3 movement = transform.forward * _input.z + transform.right * _input.x;
+    _rb.MovePosition(transform.position + movement.normalized * _speed * Time.deltaTime);
+}
+}
