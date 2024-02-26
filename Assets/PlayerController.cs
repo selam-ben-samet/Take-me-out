dce6181ab1201157a306979b3c27d82349f9cb59
selam-ben-samet <basabdussamed@gmail.com>
2024-02-26 14:14:05 +0300

    mouse a bakıyor

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
-{
-    public float speed;
+{   
+    [Header("Movement Settings")]
+    [SerializeField] private float speed;
+    
+    [SerializeField] private float _turnSpeed = 1000;
     private Vector2 move;
     public void OnMove(InputAction.CallbackContext context)
     {
@@ -22,14 +25,31 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         movePlayer();
+        LookAtMouse();
     }
 
     public void movePlayer()
     {
         Vector3 movement = new Vector3(move.x , 0f , move.y);
 
-        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f);
+        // transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f);
 
         transform.Translate(movement * speed * Time.deltaTime, Space.World);
+    }
+    void LookAtMouse()
+    {
+        // Raycast from the camera to the mouse position
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            // Calculate the relative direction from the player position to the mouse position
+            Vector3 relative = hit.point - transform.position;
+            relative.y = 0; // Set the y-component to zero to keep the rotation on the same plane
+
+            // Rotate the player towards the relative direction
+            Quaternion rotation = Quaternion.LookRotation(relative, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, _turnSpeed * Time.deltaTime);
+        }
     }
 }
