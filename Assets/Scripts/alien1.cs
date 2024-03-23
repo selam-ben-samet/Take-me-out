7935502a500fdc6c923805bf2659d6e4373cef60
samet <basabdussamed@gmail.com>
2024-03-23 21:17:18 +0300

    uzaylı artık mermi atıyor, map1 geliştirildi

--- Assets/Scripts/alien1.cs
+++ Assets/Scripts/alien1.cs
@@ -5,10 +5,29 @@ public class alien1 : MonoBehaviour
 {
     [SerializeField] private NavMeshAgent navMeshAgent;
     [SerializeField] private Transform target;
+    private Animator animator;
+    [SerializeField] private Transform bulletSpawnPoint;
+    [SerializeField] private GameObject bulletPrefab;
+
+    void Start(){
+        animator = GetComponent<Animator>();
+    }
 
     void Update(){
         if(target!=null){
             navMeshAgent.SetDestination(target.position);
+            //animator.Play("flight");
         }
+        // if(target in fov)
+        if(Input.GetKeyDown("z")){
+        animator.Play("shot");
+        Vector3 vector = target.transform.position - bulletSpawnPoint.position;
+        GameObject bullet = Instantiate(bulletPrefab,bulletSpawnPoint.position,Quaternion.identity);
+        Rigidbody bulletRigidbody = bullet.GetComponent<Rigidbody>();
+        bulletRigidbody.velocity = vector * 5f;
+                
+        }
+
+
     }
 }
